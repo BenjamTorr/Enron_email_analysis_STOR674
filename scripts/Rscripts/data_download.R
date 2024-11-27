633c054a76c8d2da5258922bfdcb5eb51ef26eef
Benjamin Torres <ibta@unc.edu>
2024-11-27 00:52:18 -0500

    Added download script, must be tested first

---
+++ scripts/Rscripts/data_download.R
@@ -0,0 +1,18 @@
+# get_enron_data: download the enron data from the url to specified folder
+# Input:
+# - data_path: The url of the data, prespecified but left open in case it changes
+# Output:
+# - None
+get_enron_data = function(data_path = 'http://www.cs.cmu.edu/~enron/enron_mail_20150507.tar.gz', 
+                    folder_path = 'data/enron_mail_20150507.tar.gz'){
+  
+  #set high timeout as data is large
+  options(timeout = 1e6)
+  print('Downloading...')
+  download.file(data_path, folder_path)
+  print('Downloaded.')
+  print('decompressing...')
+  untar(data_path, exdir = 'data/enron_mail_20150507')
+  decompresed('Done.')
+  file.remove(data_path)
+}
