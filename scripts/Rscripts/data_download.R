0ecac05ce7313901dc710c62e1f0cb24b6a62192
Benjamin Torres <ibta@unc.edu>
2024-11-28 12:12:13 -0500

    added function to download word2vec

--- scripts/Rscripts/data_download.R
+++ scripts/Rscripts/data_download.R
@@ -25,7 +25,7 @@ get_enron_data = function(data_path = 'http://www.cs.cmu.edu/~enron/enron_mail_2
   
   #set high timeout as data is large
   options(timeout = 1e6)
-  print('Downloading...')
+  cat('Please be patient, this can take a while.\n Downloading...')
   download.file(data_path, folder_path)
   print('Downloaded.')
   print('decompressing...')
@@ -33,5 +33,23 @@ get_enron_data = function(data_path = 'http://www.cs.cmu.edu/~enron/enron_mail_2
   #decompresed('Done.')
   #file.remove(data_path)
 }
+
+# get_word2vec: download the Google News Negative 300 binary file for word2vec
+# Input:
+# - data_url: url where the file can be downloaded
+# - data_path: file name after downloaded
+# Output:
+# - None
+
+get_word2vec = function(data_url = 'https://huggingface.co/NathaNn1111/word2vec-google-news-negative-300-bin/resolve/main/GoogleNews-vectors-negative300.bin?download=true', 
+                          data_path = 'GoogleNews-vectors-negative300.bin'){
+  
+  #set high timeout as data is large
+  options(timeout = 1e6)
+  cat('Please be patient, this can take a while.\n Downloading wor2vec encoding...')
+  download.file(data_url, data_path)
+  print('Downloaded.')
+}
+
 
 get_enron_data()
